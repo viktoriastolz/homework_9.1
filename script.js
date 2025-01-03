a745d48a2dc6a46636c943b0cb553c9bf84342d2
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2025-01-04 00:00:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 "use strict";
 
-function curry(c) { 
-  return function(a) {
-    return function(b) {
-      return c(a, b);
-    };
-  };
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-const curriedMultiply = curry(multiply);
-
-alert(curriedMultiply(5)(2)); // 10
 
 
 
